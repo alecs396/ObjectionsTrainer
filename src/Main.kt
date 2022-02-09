3a424f6ccc4d8f0af55ba4c044c2d48303ae1814
alecs396 <a.swainston16@gmail.com>
2022-02-08 23:20:51 -0700

    Created readFromFile function that reads the objections.txt file fully

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
+import java.io.FileReader
+
 fun main() {
-    println("Hello World")
+    readFromFile()
+}
+
+fun readFromFile() {
+
+    try {
+        var fin=FileReader("C:\\Users\\aswai\\Documents\\GitHub\\Objections Trainer\\src\\objections.txt")
+        var c : Int?
+        do {
+            c = fin.read()
+            print(c.toChar())
+        } while(c != -1)
+
+    }catch (ex:Exception) {
+        print(ex.message)
+    }
+
 }
