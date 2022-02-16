34e18959fff4de5b43c54b724b6eeddb05054ecb
alecs396 <a.swainston16@gmail.com>
2022-02-15 20:43:55 -0700

    Created Custom Objection option in menu

---
+++ src/Display.kt
@@ -0,0 +1,7 @@
+class Display {
+    fun mainMenu() {
+        println("OBJECTIONS TRAINER")
+        println("===========================")
+        println("1) Receive an Objection")
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,39 @@
 import java.io.FileReader
+import java.io.FileWriter
 
 fun main() {
     println("OBJECTIONS TRAINER")
     println("===========================")
     println("1) Receive an Objection")
+    println("2) Create Custom Objection")
 
     var op = readLine()!!.toInt()
 
     when(op) {
         1 -> {
             readFromFile()
+        } 2 -> {
+            print("Enter Custom Objection:")
+            var  str:String = readLine().toString()
+            writeToFile(str)
         }
+    }
+}
+
+fun writeToFile(str:String) {
+    try {
+        var fo = FileWriter("src\\objections.txt", true)
+        fo.write(str + "\n")
+        fo.close()
+    }catch (ex:Exception) {
+        print(ex.message)
     }
 }
 
 fun readFromFile() {
 
     try {
-        var fin=FileReader("src\\objections.txt")
+        var fin= FileReader("src\\objections.txt")
         var c : Int?
         do {
             c = fin.read()
